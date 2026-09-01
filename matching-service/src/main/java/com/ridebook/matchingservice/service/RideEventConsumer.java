5c5e0f940cce0a38c3c7ad0f181f904627177eb3
Saif Ali <alisaif7863@gmail.com>
2026-09-02 02:43:40 +0530

    feat: configure Kafka and add ride event consumer

--- matching-service/src/main/java/com/ridebook/matchingservice/service/RideEventConsumer.java
+++ matching-service/src/main/java/com/ridebook/matchingservice/service/RideEventConsumer.java
@@ -1,4 +1,39 @@
 package com.ridebook.matchingservice.service;
 
+import com.ridebook.matchingservice.event.RideRequestedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class RideEventConsumer {
+
+    private final MatchingService matchingService;
+
+    /**
+     * Listens to ride.requested Kafka topic
+     * Triggered everyy time ride service publishes a new ride request
+     *
+     * Flow:
+     * Ride Service >> Kafka (ride.requested) >> This Consumer >> MatchingService
+     */
+
+    @KafkaListener(
+            topics = "ride.requested",
+            groupId = "matching-service-group"
+    )
+    public void consumeRideRequestedEvent(RideRequestedEvent event) {
+        try {
+            matchingService.matchedDriverForRide(event);
+        } catch (Exception e) {
+            log.error(
+                    "Error processing ride request: {} - {}",
+                    event.getRideId(),
+                    e.getMessage()
+            );
+        }
+    }
 }
